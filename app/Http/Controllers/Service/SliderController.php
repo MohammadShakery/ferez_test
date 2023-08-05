7576dda8e3ad27e983f2fa94cecf89bc2065a127
csbay <cs.baymohammadi@gmail.com>
2023-08-05 14:38:38 +0330

    fix bug on slider api service

--- app/Http/Controllers/Service/SliderController.php
+++ app/Http/Controllers/Service/SliderController.php
@@ -8,11 +8,11 @@ use Illuminate\Http\Request;
 
 class SliderController extends Controller
 {
-    public function show(string $location)
+    public function show(Request $request)
     {
         return response([
             'status' => true ,
-            'sliders' => Slider::query()->where('location',$location)
+            'sliders' => Slider::query()->where('location',$request->get('location'))
                 ->orderByDesc('priority')->limit(5)->get()
         ],200);
     }
