22f723952f519cd535a52dff653d3e21ce68e12f
csbay <cs.baymohammadi@gmail.com>
2023-08-13 05:35:10 +0330

    setup test api for get brands from api is done

--- routes/web.php
+++ routes/web.php
@@ -19,34 +19,34 @@ use Illuminate\Support\Facades\Storage;
 
 Route::get('/service/amp',[\App\Http\Controllers\TestController::class,'test']);
 
-Route::get('test',function (){
-    $string = "https://panel.sanatabzar128.ir/uploads/1660983551.png";
-    $client = new Client();
-    $headers = [
-    ];
-    $request = new Request('GET', 'https://sanatabzar128.ir/mbm', $headers);
-    $res = $client->send($request);
-    $data =  json_decode($res->getBody());
-    $brands = $data->brands;
-    foreach ($brands as $brand)
-    {
-        if($brand->is_delete == "0")
-        {
-                $path = $brand->img;
-                $name = explode('/',$brand->img);
-                Storage::put('public/brands/'.$name[4],file_get_contents($path));
-                $brand2 = \App\Models\Brand::query()->create([
-                    'name' => $brand->name ,
-                    'description' => $brand->body,
-                    'tell' => $brand->phone ,
-                    'image' => 'storage/brands/'.$name[4] ,
-                    'address' => $brand->insta
-                ]);
-
-
-
-
-        }
-    }
-
-});
+//Route::get('test',function (){
+//    $string = "https://panel.sanatabzar128.ir/uploads/1660983551.png";
+//    $client = new Client();
+//    $headers = [
+//    ];
+//    $request = new Request('GET', 'https://sanatabzar128.ir/mbm', $headers);
+//    $res = $client->send($request);
+//    $data =  json_decode($res->getBody());
+//    $brands = $data->brands;
+//    foreach ($brands as $brand)
+//    {
+//        if($brand->is_delete == "0")
+//        {
+//                $path = $brand->img;
+//                $name = explode('/',$brand->img);
+//                Storage::put('public/brands/'.$name[4],file_get_contents($path));
+//                $brand2 = \App\Models\Brand::query()->create([
+//                    'name' => $brand->name ,
+//                    'description' => $brand->body,
+//                    'tell' => $brand->phone ,
+//                    'image' => 'storage/brands/'.$name[4] ,
+//                    'address' => $brand->insta
+//                ]);
+//
+//
+//
+//
+//        }
+//    }
+//
+//});
