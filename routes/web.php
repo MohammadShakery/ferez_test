bc5903c778f67ce1eba69e85ee22e91cdc625334
csbay <cs.baymohammadi@gmail.com>
2023-10-19 02:08:18 +0330

    load balance test with redis cache

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
+use App\Models\Category;
 use App\Services\SMS\SmsService;
 use Aws\Exception\AwsException;
 use Aws\Exception\MultipartUploadException;
 use Aws\S3\Exception\S3Exception;
 use GuzzleHttp\Client;
 use GuzzleHttp\Psr7\Request;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Storage;
 
@@ -21,9 +23,19 @@ use Illuminate\Support\Facades\Storage;
 */
 
     Route::get('/test',function (){
+        if(Cache::has('categories'))
+        {
+            $data_array = (array)json_decode(Cache::get('categories'));
+            $data_array["cache"] = true;
+            return response($data_array,200);
+        }
+        $data =array(
+            'status'         => true ,
+            'categories' => \App\Models\Category::all());
+        Cache::put('categories',json_encode($data),now()->addSeconds(300));
        return response([
            'status' => true ,
-           'categories' => \App\Models\Category::all()
+           'categories' => $data
        ],200);
     });
 
