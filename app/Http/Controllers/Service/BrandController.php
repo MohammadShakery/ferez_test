d6817e35df745dbe3ee1d942eeeedfb2163fa35c
csbay <cs.baymohammadi@gmail.com>
2023-12-05 16:11:15 +0330

    add paginate to searchs

--- app/Http/Controllers/Service/BrandController.php
+++ app/Http/Controllers/Service/BrandController.php
@@ -65,7 +65,7 @@ class BrandController extends Controller
     {
         return response([
             'status' => true ,
-            'brands' => Brand::query()->where('name','like',"%{$slug}%")->simplePaginate(10)
+            'brands' => Brand::query()->where('name','like',"%{$slug}%")->paginate(10)
         ],200);
     }
 
@@ -73,7 +73,7 @@ class BrandController extends Controller
     {
         return response([
             'status' => true ,
-            'products' => Product::query()->where('name','like',"%{$slug}%")->simplePaginate(10)
+            'products' => Product::query()->where('name','like',"%{$slug}%")->paginate(10)
         ],200);
     }
 
