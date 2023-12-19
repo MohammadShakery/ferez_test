a7912a91e4116f3e43cb6dbc40e555616786d698
csbay <cs.baymohammadi@gmail.com>
2023-12-19 13:22:14 +0330

    add sentry monitor to schedules

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
          $schedule->command('app:visitors')->hourly();
-         $schedule->command('app:price')->everyFiveMinutes();
+         $schedule->command('app:price')->everyFiveMinutes()->sentryMonitor();;
          $schedule->command('brand:view')->everyFifteenMinutes();
     }
 
